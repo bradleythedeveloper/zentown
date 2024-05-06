35d155ff8aa3079fbd1faf6521d728e645579f46
BradsTheDeveloper <bradleyaustin2611@gmail.com>
2024-05-06 13:23:43 +0100

    Add time and date, start widget foundation

---
+++ app/ui/widget.js
@@ -0,0 +1,10 @@
+export default function Widget({widget}) {
+    /*if (widget === "spotify") {
+        return (
+            <h1>Spotify</h1>
+        )
+    }*/
+    return (
+        <h1>{widget.name}</h1>
+    )
+}
